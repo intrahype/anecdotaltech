de59ca4af7b31e3e3e768c8be0b140a78637509f
Kenyon Fryman <54225943+intrahype@users.noreply.github.com>
2024-06-23 15:23:21 -0700

    comment edits

--- PersonalFunctions/pomodorotasks.py
+++ PersonalFunctions/pomodorotasks.py
@@ -2,7 +2,7 @@
 import random
 
 #create list of task items and number of iterations
-#if you are using this for yourself, just copy and change out the tasks and add as many cycle counts as you want
+#to use this for yourself, just copy and change out the tasks and add the cycle counts you prefer
 tasks = ["3D Print", "Wood Carving", "Crochet", "Sewing", "Chainmail", "Code"]
 
 cycles = [1, 2, 3, 4]
