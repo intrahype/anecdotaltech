c7edb98852673dab60e6b484cd0a54dc4e47838b
Kenyon Fryman <54225943+intrahype@users.noreply.github.com>
2024-06-23 15:43:40 -0700

    added collatz script

---
+++ PersonalFunctions/collatzScript.py
@@ -0,0 +1,16 @@
+#work in progress from Automate the Boring Stuff Chapter 3
+
+#a small script to run the collatz sequence for fun
+
+print("Enter number")
+
+number = input()
+
+def collatz(number)
+    if number % 2 == 0
+        number = number / 2
+        return number
+    if number == 1
+        sys.exit()
+    else
+        number = 3 * number + 1
