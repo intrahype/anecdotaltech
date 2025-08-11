9a190841a96c88b05ca43b64762ebf3e67b3ccd1
Kenyon Fryman <54225943+intrahype@users.noreply.github.com>
2025-08-10 21:29:50 -0700

    added first programs to 140 python

---
+++ 140 Simple Python Programs/program01_HelloPython.py
@@ -0,0 +1,2 @@
+#Write a program to print "Hello Python"
+print("Hello Python!")
---
+++ 140 Simple Python Programs/program02_ArithmeticalOperations.py
@@ -0,0 +1,8 @@
+#Write a Python program to do arithmetical Operations
+
+def addition(self, num1, num2):
+    num1 = float(input("Enter an integer for addition: "))
+    num2 = float(input("Enter another integer for addition: "))
+    integer_sum = num1 + num2
+    return print(f"sum: {num1} + {num2} = {integer_sum}")
+
